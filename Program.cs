570184261f5bb6565c59b09b11281537d3d967ee
juanjmeza <80909973+juanjmeza@users.noreply.github.com>
2021-03-25 13:06:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A877340.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+    }
+}
